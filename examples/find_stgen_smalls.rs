e8f79f5e85092f55bacf5288b03a0c9d1987ff79
Thom Wiggers <thom@thomwiggers.nl>
2018-11-19 14:14:04 +0100

    initial version of finding-codes script with complexity

---
+++ examples/find_stgen_smalls.rs
@@ -0,0 +1,82 @@
+#![feature(box_syntax)]
+extern crate lpn;
+extern crate rand;
+#[macro_use]
+extern crate lazy_static;
+
+use std::env;
+
+use lpn::codes::*;
+
+const STGENS: usize = 2;
+const W0: u32 = 2;
+const WB: u32 = 2;
+const WINC: u32 = 1;
+const MAX_L: usize = 200;
+
+lazy_static! {
+    static ref IDENTITIES: Vec<IdentityCode> = (1..=4).into_iter().map(IdentityCode::new).collect();
+    static ref REPETITIONS: Vec<RepetitionCode> =
+        (2..=4).into_iter().map(RepetitionCode::new).collect();
+}
+
+fn generate_codes(n_max: usize) {
+    let mut codes: Vec<&dyn BinaryCode> =
+        vec![&MdsCode3_2, &MdsCode4_3, &MdsCode5_4, &CustomCode5_3];
+    codes.reserve(IDENTITIES.len() + REPETITIONS.len());
+    for rep in REPETITIONS.iter() {
+        codes.push(rep);
+    }
+
+    generate_codes_all_same(&codes, n_max);
+
+    for id in IDENTITIES.iter() {
+        codes.push(id);
+    }
+}
+
+/// This uses exhaustive search, complexity is O(|codes|!)
+///
+/// n: number of codes
+fn generate_codes_all_same<'c>(codes: &Vec<&'c dyn BinaryCode>, max_n: usize) {
+    let mut bests = Vec::with_capacity(codes.len() * max_n);
+    for code in codes.iter() {
+        for n in 1..=max_n {
+            let blocks = std::iter::repeat(*code).take(n).collect::<Vec<_>>();
+            let best_stgen = (0..STGENS)
+                .into_iter()
+                .map(|_| {
+                    let code = StGenCode::new(blocks.clone(), W0, MAX_L, WB, WINC);
+                    println!("Measuring {}", code.name());
+                    let bias = code.bias(0.75);
+                    (code, bias)
+                })
+                .max_by(|t1, t2| {
+                    if (t1.1).lt(&t2.1) {
+                        std::cmp::Ordering::Less
+                    } else {
+                        std::cmp::Ordering::Greater
+                    }
+                })
+                .unwrap();
+            bests.push(best_stgen);
+        }
+    }
+    for (code, bias) in bests.into_iter() {
+        let json = serde_json::to_string(&code).unwrap();
+        println!("{}\t{}\t{}\t{}", code.name(), bias, code.decoding_complexity(), json);
+    }
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("Got args: {:?}", args);
+        panic!("Insufficient args. Expect ./{} n_max", args[0]);
+    }
+
+    let n_max = args[1].parse::<usize>().expect("Needs to be int");
+
+    generate_codes(n_max);
+}
