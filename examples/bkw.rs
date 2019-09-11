de6054aab1c5bbee6e25f4678caf17bfe920b16a
Thom Wiggers <thom@thomwiggers.nl>
2019-09-11 08:33:08 +0200

    Switch a, b in BKW example to make it more reliable

--- examples/bkw.rs
+++ examples/bkw.rs
@@ -4,11 +4,14 @@ use lpn::bkw::*;
 use lpn::oracle::LpnOracle;
 
 fn main() {
+    let a = 4;
+    let b = 8;
+
     let mut oracle: LpnOracle = LpnOracle::new(32, 1.0 / 32.0);
-    oracle.get_samples(400_555);
+    oracle.get_samples(2usize.pow(20));
     let mut secret = oracle.secret.clone();
-    secret.truncate(oracle.k as usize);
-    let solution = bkw(oracle, 8, 4);
+    secret.truncate((oracle.k - (a-1)*b) as usize);
+    let solution = bkw(oracle, a, b);
 
     println!("Found:  {:?}", solution);
     println!("Actual: {:?}", secret);
