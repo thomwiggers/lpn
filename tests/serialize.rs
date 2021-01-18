4cd88745e372a12670ceb8b16f73282934a0fb25
Thom Wiggers <thom@thomwiggers.nl>
2021-01-18 16:48:30 +0100

    feature-guard test

--- tests/serialize.rs
+++ tests/serialize.rs
@@ -4,18 +4,21 @@ extern crate rayon;
 extern crate serde;
 extern crate serde_json;
 
-use lpn::codes::*;
+#[cfg(feature = "codes")]
+mod codes {
+    use lpn::codes::*;
 
-fn get_code() -> StGenCode<'static> {
-    let codes: Vec<&dyn BinaryCode> = vec![&HammingCode7_4, &HammingCode7_4, &HammingCode7_4];
-    StGenCode::new(codes, 3, 100, 3, 2)
-}
+    fn get_code() -> StGenCode<'static> {
+        let codes: Vec<&dyn BinaryCode> = vec![&HammingCode7_4, &HammingCode7_4, &HammingCode7_4];
+        StGenCode::new(codes, 3, 100, 3, 2)
+    }
 
-#[test]
-fn test_serialization() {
-    let code = get_code();
-    println!("Code: [{}, {}]", code.length(), code.dimension());
+    #[test]
+    fn test_serialization() {
+        let code = get_code();
+        println!("Code: [{}, {}]", code.length(), code.dimension());
 
-    // serialize
-    println!("{}", serde_json::to_string(&code).unwrap());
+        // serialize
+        println!("{}", serde_json::to_string(&code).unwrap());
+    }
 }
